90c7417941cb2abedcef409b56af806430bea2c9
blindlight86 <blindlight1986@gmail.com>
2023-07-12 16:43:02 +0800

    change platform to amd64

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@
 # Set PLATFORM as required for your router model. See:
 # https://mikrotik.com/products/matrix
 #
-PLATFORM="linux/arm/v7"
+PLATFORM="linux/AMD64"
 TAILSCALE_VERSION=1.44.0
 VERSION=0.1.18
 
